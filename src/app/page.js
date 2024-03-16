647a2dedcedd45460e45c9f351fdae42955af928
Ekimerton <ekim0252@gmail.com>
2024-03-16 18:00:34 -0400

    Quick fix for api not working

--- src/app/page.js
+++ src/app/page.js
@@ -18,20 +18,37 @@ async function getRandomWordFromFile() {
 async function getDailyWord(dayNumber) {
   const filePath = path.join(process.cwd(), "./public/chosenWords.txt");
   const data = fs.readFileSync(filePath, "utf8");
-  const words = data.split("\n");
+  const lines = data.split("\n");
   const index = dayNumber - 1;
-  if (index >= 0 && index < words.length) {
-    return words[index].trim();
+
+  if (index >= 0 && index < lines.length) {
+    const line = lines[index].trim();
+    // Check if the line has synonyms (enclosed in square brackets)
+    if (line.includes("[")) {
+      const [word, synonymsStr] = line.split("[");
+      const synonyms = synonymsStr.slice(0, -1).split(", ");
+      return { word: word.trim(), synonyms };
+    } else {
+      const word = line;
+      // Fetch synonyms if they are not defined
+      const synonyms = await getSynonyms(word);
+      return { word, synonyms };
+    }
   } else {
-    return await getRandomWordFromFile();
+    // Assuming getRandomWordFromFile is adjusted accordingly
+    const randomWord = await getRandomWordFromFile();
+    if (randomWord.synonyms.length === 0) {
+      // Fetch synonyms if not available
+      randomWord.synonyms = await getSynonyms(randomWord.word);
+    }
+    return randomWord;
   }
 }
 
 export default async function Home() {
   const cookieStore = cookies();
   const number = getGameNumber();
-  const word = await getDailyWord(number);
-  const synonyms = await getSynonyms(word);
+  const { word, synonyms } = await getDailyWord(number);
   const definition = await getWordDefinition(word);
   return (
     <Suspense>
